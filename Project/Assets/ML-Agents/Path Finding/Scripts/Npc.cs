f67eaef2c83a857e52b5a754798767e8e95a896f
aomazic <toniomazic7@gmail.com>
2024-03-28 14:17:51 +0100

    more stuff

---
+++ Project/Assets/ML-Agents/Path Finding/Scripts/Npc.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class Npc : Agent
+{
+    public GameObject area;
+    private TestArea testArena;
+    private Rigidbody npcRigidBody;
+    [SerializeField] private float moveSpeed = 1000f;
+
+    public override void Initialize()
+    {
+        npcRigidBody = GetComponent<Rigidbody>();
+        testArena = area.GetComponent<TestArea>();
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(testArena.goal.transform.position);
+        sensor.AddObservation(transform.InverseTransformDirection(npcRigidBody.velocity));
+    }
+
+    private void MoveAgent(ActionSegment<int> act)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var action = act[0];
+        switch (action)
+        {
+            case 1:
+                dirToGo = transform.forward * 1f;
+                break;
+            case 2:
+                dirToGo = transform.forward * -1f;
+                break;
+            case 3:
+                rotateDir = transform.up * 1f;
+                break;
+            case 4:
+                rotateDir = transform.up * -1f;
+                break;
+            case 5:
+                dirToGo = Vector3.zero;
+                break;
+        }
+        transform.Rotate(rotateDir, Time.deltaTime * 150f);
+        npcRigidBody.AddForce(dirToGo * moveSpeed, ForceMode.Force);
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        AddReward(-1f / MaxStep);
+        MoveAgent(actionBuffers.DiscreteActions);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut[0] = 5;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[0] = 3;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[0] = 4;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        testArena.ResetArea();
+
+        npcRigidBody.velocity = Vector3.zero;
+        transform.rotation = Quaternion.Euler(new Vector3(0f, Random.Range(0, 360)));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("goal"))
+        {
+            return;
+        }
+        SetReward(2f);
+        EndEpisode();
+    }
+}
---
+++ Project/Assets/ML-Agents/Path Finding/Scripts/TestArea.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TestArea : MonoBehaviour
+{
+    [SerializeField] private GameObject[] props;
+    [SerializeField] private GameObject[] spawnAreas;
+    [SerializeField] private GameObject floor;
+    public GameObject goal;
+
+    // Create a dictionary to store the bounds of each spawn area
+    private Dictionary<string, Bounds> spawnAreaBounds;
+    private Bounds floorSize;
+
+    private void Start()
+    {
+        floorSize = floor.GetComponent<Renderer>().bounds;
+
+        spawnAreaBounds = new Dictionary<string, Bounds>();
+
+        foreach (var spawnArea in spawnAreas)
+        {
+            var spawnAreaRenderer = spawnArea.GetComponent<Renderer>();
+            if (spawnAreaRenderer != null)
+            {
+                spawnAreaBounds.Add(spawnArea.name, spawnAreaRenderer.bounds);
+            }
+            else
+            {
+                Debug.LogError("Renderer component is not attached to " + spawnArea.name);
+            }
+        }
+    }
+
+    public void ResetArea()
+    {
+        CleanArena();
+        SpawnProps();
+        goal.transform.position = new Vector3(
+            Random.Range(floorSize.min.x, floorSize.max.x),
+            0f,
+            Random.Range(floorSize.min.z, floorSize.max.z)
+        );
+    }
+
+    private void SpawnProps()
+    {
+        foreach (var spawnArea in spawnAreas)
+        {
+            // Get the cached bounds of the spawn area
+            var bounds = spawnAreaBounds[spawnArea.name];
+
+            foreach (var prop in props)
+            {
+                // Generate a random position within the bounds of the spawn area
+                var position = new Vector3(
+                    Random.Range(bounds.min.x, bounds.max.x),
+                    0f,
+                    Random.Range(bounds.min.z, bounds.max.z)
+                );
+
+                var rotation = Quaternion.Euler(0f, Random.Range(0, 360), 0f);
+                Instantiate(prop, position, rotation, transform);
+            }
+        }
+    }
+
+    private void CleanArena()
+    {
+        foreach (Transform child in transform)
+            if (child.CompareTag("Prop") || child.CompareTag("MovableProp"))
+            {
+                Destroy(child.gameObject);
+            }
+    }
+}
